d779a401f10eb8b4f944578cd66eec1a35bd86f1
alexanderchao123 <chao.alexander.123@gmail.com>
2018-09-15 04:10:42 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,6 +1,9 @@
 require 'pry'
-
 def alphabetize(arr)
   alphabets = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  arr.sort_by {|word| word.each_char.map {|char| alphabets.index(char)}}
+
+  arr.sort_by do |word|
+    word.each_char.map {|char| alphabets.index(char)}
 end
+
+alphabetize(["bonan matenon", "bonan vesperon", "bonan nokton"])
